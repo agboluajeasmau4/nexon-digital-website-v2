64ad8bc743a7547b5e18137d4c39c7801b81f8e2
agboluajeasmau4 <agboluajeasmau4@gmail.com>
2026-01-30 22:42:32 +0100

    html problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-// import React from "react";
-// import Home from "./pages/Home";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Home />
-//     </div>
-//   );
-// }
-
-// export default App;
-import React from "react";
+import React from "react";  
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ServicePage from "./pages/Services";
 import Subscribe from "./pages/Subscribe";
 
-
 function App() {
   return (
-    <Router>
+    <Router basename="/nexon-digital-website-v2">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<ServicePage />} />
-         <Route path="/subscribe" element={<Subscribe />} />
-       
+        <Route path="services" element={<ServicePage />} />
+        <Route path="subscribe" element={<Subscribe />} />
+        <Route path="*" element={<Home />} /> {/* fallback for unknown paths */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
